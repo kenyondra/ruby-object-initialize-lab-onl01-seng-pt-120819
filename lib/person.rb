3e5a0b614d4ee2e0ded066096cece6e0d796dec9
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2020-01-18 02:43:15 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,13 +3,13 @@ class Person
     @name = name 
   end
   
-  def name=(name)
-    @name = name
-  end
+  #def name=(name)
+   # @name = name
+  #end
   
-  def name 
-    @name
-  end 
+  #def name 
+   # @name
+  #end 
 end   
 
 beyonce = Person.new("Beyonce")
