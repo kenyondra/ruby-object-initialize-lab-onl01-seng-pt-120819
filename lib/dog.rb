67f8fed60a03e131bcda30b37004eea4986f407e
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2020-01-18 02:14:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,13 @@
 class Dog 
-  def initialize(name)
-   @name = name 
-  end
-  
-  #def initialize(breed)
-   # @breed = breed
+  #def initialize(name)
+   #@name = name 
   #end
+  
+  def initialize(breed)
+   @breed = breed
+  end
 end   
 
-fido = Dog.new("Fido", "Pug") 
+fido = Dog.new("Fido")
+fido.breed 
 
